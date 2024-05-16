b58b34eca7210467040d9a3f8b79c8199d3277f7
VitorVieira <vitorhvvieira@gmail.com>
2024-05-16 14:09:17 -0300

    finalizando service e começando o controller

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -0,0 +1,33 @@
+package br.com.alurafood.pagamentos.controller;
+
+import br.com.alurafood.pagamentos.dtos.PagamentoDto;
+import br.com.alurafood.pagamentos.service.PagamentoService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pagamentos")
+public class PagamentoController {
+
+    private PagamentoService service;
+
+    @GetMapping
+    public Page<PagamentoDto> listar(@PageableDefault(size = 10) Pageable paginacao) {
+        return service.obterTodos(paginacao);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PagamentoDto> detalhar(@PathVariable @NotNull Long id) {
+        PagamentoDto dto = service.obterPorId(id);
+        return ResponseEntity.ok(dto);
+    }
+
+}
